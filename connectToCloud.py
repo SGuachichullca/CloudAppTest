70333517ddfeeb4a81145c9b1daf966adfba42bc
SGuachichullca <sguachichullca@gmail.com>
2023-07-28 13:52:46 -0400

    Deployment Attempt 3

--- connectToCloud.py
+++ connectToCloud.py
@@ -34,27 +34,3 @@ if __name__ == '__main__':
     app.secret_key = '123123'
     app.debug = True
     app.run()
-
-
-'''def getVidTitle():
-    chrome_options = Options()
-
-    chrome_options.add_experimental_option("detach", True)
-    #chrome_options.add_argument('--headless')
-    #chrome_options.add_argument('--start-minimized')  # Start Chrome maximized (full screen)
-    chrome_options.add_argument('--disable-infobars')  # Disable the "Chrome is being controlled by automated test software" notification
-
-    global driver
-    driver = webdriver.Chrome(options=chrome_options)
-    try:
-        driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        time.sleep(5)
-        button_element = driver.find_element(By.ID, "subscribe-button")
-        print(button_element.text)
-        button_element.click()
-
-        driver.quit()
-        return()
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return(f"Error: {str(e)}")'''
