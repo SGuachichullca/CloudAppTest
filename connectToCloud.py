541be779ed0ea3678381fc21b17ee1cdfcdc6e55
SGuachichullca <sguachichullca@gmail.com>
2023-07-28 15:31:53 -0400

    Deployment Attempt 11

--- connectToCloud.py
+++ connectToCloud.py
@@ -5,9 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-import gunicorn
-import glob
-import os 
 
 app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
