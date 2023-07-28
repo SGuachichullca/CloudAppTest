0ad458c98aa570b0d23b26543790146a9d5a9d17
SGuachichullca <sguachichullca@gmail.com>
2023-07-28 15:29:58 -0400

    Deployment Attempt 10

--- connectToCloud.py
+++ connectToCloud.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import gunicorn
 import glob
 import os 
 
