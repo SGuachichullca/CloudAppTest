f1cd15ecd5238109b47f44e0ada11fd40a0a7902
SGuachichullca <sguachichullca@gmail.com>
2023-08-02 11:09:54 -0400

    Deployment Attempt 13

--- connectToCloud.py
+++ connectToCloud.py
@@ -6,30 +6,36 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-app = Flask(__name__)
-@app.route('/', methods = ['GET', 'POST'])
+#app = Flask(__name__)
+#@app.route('/', methods = ['GET', 'POST'])
 #def test():
 #   return('test')
 
-def getVidTitle():
-    chrome_options = Options()
-    global driver
-    driver = webdriver.Chrome(options=chrome_options)
+#ef getVidTitle():
+chrome_options = Options()
+global driver   
+chrome_options.set_capability("browserVersion", "104")
+chrome_options.set_capability("platformName", "Windows 11")
+
+driver = webdriver.Remote(
+    options=chrome_options,\
     
-    try:
-        driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        time.sleep(5)
-        button_element = driver.find_element(By.ID, "subscribe-button")
-        button_element.click()
-        time.sleep(5)
-        driver.close()
-        return('')
+    )
 
-    except Exception as e:
-        return(f"Error: {str(e)}")
+try:
+    driver.get('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    time.sleep(5)
+    button_element = driver.find_element(By.ID, "subscribe-button")
+    button_element.click()
+    time.sleep(5)
+    driver.close()
+    print('Done.')
 
-if __name__ == '__main__':
+except Exception as e:
+    print(f"Error: {str(e)}")
+
+#if __name__ == '__main__':
     app.secret_key = '123123'
     app.debug = True
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='192.168.1.214', port='5000')
 
