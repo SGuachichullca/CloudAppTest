354577ae9e45b482438c0f687f603103a2def0fc
SGuachichullca <sguachichullca@gmail.com>
2023-08-04 09:36:20 -0400

    Attempting Selenium using Heroku 10

--- connectToCloud.py
+++ connectToCloud.py
@@ -4,21 +4,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.service import Service as ChromeService
+
 import time
 import os
-from pyppeteer import launch
 
 app = Flask(__name__)
 chrome_options = webdriver.ChromeOptions()
+chrome_service = ChromeService(executable_path='CHROMEDRIVER_PATH')
+
 
 @app.route('/', methods=['GET', 'POST'])
 
 def main():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(service=os.environ.get('CHROMEDRIVER_PATH'), options=chrome_options)
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     try:
         driver.get('https://stackoverflow.com/')
         src = driver.page_source
