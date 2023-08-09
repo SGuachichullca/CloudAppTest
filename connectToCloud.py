0010a1f7586e3607cedb3275e491c80c0725c10d
SGuachichullca <sguachichullca@gmail.com>
2023-08-09 08:56:14 -0400

    Attempting a more complex selenium script

--- connectToCloud.py
+++ connectToCloud.py
@@ -40,11 +40,37 @@ def main():
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
     try:
-        driver.get("https://stackoverflow.com/")
+        '''driver.get("https://stackoverflow.com/")
         src = driver.page_source
         driver.quit()
         print("Done.")
-        return(src)
+        return(src)'''
+        driver.get('https://fires.fdnycloud.org/CitizenAccess/Cap/CapHome.aspx?module=BFP&TabName=BFP')
+
+        search_bar = driver.find_element(By.NAME, "ctl00$PlaceHolderMain$generalSearchForm$txtGSPermitNumber")
+        search_bar.click()
+        search_bar.send_keys('42091108')
+        search_bar.send_keys(Keys.ENTER)
+        #actions.click(search_bar).send_keys(VONum).send_keys(Keys.ENTER).perform()
+        time.sleep(3)
+
+        record_address = driver.find_element(By.CLASS_NAME, "NotBreakWord").text
+        #print(record_address.text)
+        record_info = driver.find_element(By.CLASS_NAME, "selected")
+        supp_documents = driver.find_element(By.CLASS_NAME, "dropdown-menu")
+        #actions.click(record_info).click(supp_documents).perform()
+        record_info.click()
+        supp_documents.click()
+
+        download_link = driver.find_element(By.ID, "ctl00_PlaceHolderMain_attachmentEdit_iframeAttachmentList")
+        driver.get(download_link.get_property('src'))
+
+        download_link = driver.find_element(By.ID, 'attachmentList_gdvAttachmentList_ctl02_lblName')
+        #actions.click(download_link).perform()
+        download_link.click()
+        time.sleep(3)
+    
+        driver.quit()
     
     except Exception as e:
         # Handle any exceptions that occur
